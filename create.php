559e82159b7f0980195f63e4ea46b3a98d28217d
Joseph Atkins-Turkish <joe@Josephs-MacBook-Pro.local>
2014-02-09 12:25:17 +0000

    Fixed urls

--- create.php
+++ create.php
@@ -6,10 +6,10 @@ head();
 <h2> Game builder </h2>
 <p> Write your story in the text box below. Any words inside [tags] will be
     replaced by users of your story, with the content inside used as a prompt (see
-    <a href="/wordgame/tags.php">this page</a> for more information about the tags). For example:
+    <a href="/tags.php">this page</a> for more information about the tags). For example:
 </p>
 <q> One [adjective] [season | summer]'s day, [name] decided to [infinitive verb] her [noun] with her toilet.</q>
-<form method="POST" action="/wordgame/post.php">
+<form method="POST" action="/post.php">
     <textarea cols="30" rows="8" id="storytextarea" name="content" required placeholder="Write your [adjective] story here."></textarea> <br />
     <input type="text" name="name" id="nameinput" required /> <label for="nameinput"> Story name (must be unique, used for the URL) </label><br/>
     <input type="text" name="author" id="authorinput" value="Anonymous" required /> <label for="authorinput"> Your name/nickname </label><br/>
